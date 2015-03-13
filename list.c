b6f3ed89e4cf349035a028ce33db478657cc2660
Julian Wolf <julian.wolf@mail.mcgill.ca>
2015-03-13 18:08:40 -0400

    added prettyPrint() and fixed things

--- list.c
+++ list.c
@@ -1,17 +1,32 @@
-#include <list.h>
+#include "list.h"
 
-void add (int) {
+NODE *head;
 
+#define NEW_NODE (NODE *) malloc(sizeof(NODE))
+
+void add (int val) {
+	if (head == 0) {
+		head = NEW_NODE;
+		head->next = 0;
+	}
+
+	NODE *crt = head;
+	while (crt->next != 0) crt = crt->next;
+	
+	NODE *new_node = NEW_NODE;
+	crt->next = new_node;
+	new_node->val = val;
+	new_node->next = 0;
 }
 
 void prettyPrint (void) {
 
 }
 
-BOOLEAN delete (int) {
+BOOLEAN delete (int val) {
 
 }
 
-struct NODE *find (int) {
+struct NODE *find (int val) {
 
 }
