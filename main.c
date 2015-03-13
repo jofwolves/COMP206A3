7018f7149e58f10ac14d83d796646dad448032a0
Julian Wolf <julian.wolf@mail.mcgill.ca>
2015-03-13 17:12:19 -0400

    added all functional parts of main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "list.h"
+#include <stdlib.h>
 
 int main (void) {
 	
@@ -12,6 +12,34 @@ int main (void) {
 		numbers = fopen(fname,"rt");
 	} while (errno != 0);
 
+	int number; // holds each number to be added or deleted
+	while (fscanf(numbers,"%d\n",&number) == 1) {
+		add(number); // add numbers to list
+	}
+
+	prettyPrint(); // print numbers which have been added
+
+	do {
+		int keep_deleting = 0;
+		printf("Input a number to be deleted from the list:\n");
+		scanf("%d",&number);
+
+		has_been_deleted = delete(number);
+		if (has_been_deleted) printf("NUMBER WAS DELETED");
+		else printf("NUMBER WAS NOT FOUND");
+
+		prettyPrint();
+
+		char delete_another[20];
+		printf("Would you like to delete another number?");
+		scanf("%s",delete_another);
+		if (!strcmp(delete_another,"yes") ||
+			!strcmp(delete_another,"YES") ||
+			!strcmp(delete_another,"Yes") ||
+			!strcmp(delete_another,"y") ||
+			!strcmp(delete_another,"Y")) keep_deleting = 1;
+
+	} while (keep_deleting);
 
 	fclose(numbers);
 	return 0;
